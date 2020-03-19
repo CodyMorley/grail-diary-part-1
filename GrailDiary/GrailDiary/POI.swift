d60e06946812daf2e7c207f2de6bb4407080ca9b
Cody Morley <codymorleyinc@gmail.com>
2020-03-19 12:45:08 -0700

    finished creating new class files

---
+++ GrailDiary/GrailDiary/POI.swift
@@ -0,0 +1,9 @@
+//
+//  POI.swift
+//  GrailDiary
+//
+//  Created by Cody Morley on 3/19/20.
+//  Copyright © 2020 Cody. All rights reserved.
+//
+
+import Foundation
---
+++ GrailDiary/GrailDiary/POITableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  POITableViewCell.swift
+//  GrailDiary
+//
+//  Created by Cody Morley on 3/19/20.
+//  Copyright © 2020 Cody. All rights reserved.
+//
+
+import UIKit
+
+class POITableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
