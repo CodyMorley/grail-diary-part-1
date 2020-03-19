9dc536a319ac5829de2113be8561849d3d11b04f
Cody Morley <codymorleyinc@gmail.com>
2020-03-19 15:53:15 -0700

    not finished but already late for Q&A

--- GrailDiary/GrailDiary/AddPOIViewController.swift
+++ GrailDiary/GrailDiary/AddPOIViewController.swift
@@ -8,8 +8,47 @@
 
 import UIKit
 
+protocol AddPOIDelegate {
+    func poiWasAdded(_ poi: POI)
+}
+
 class AddPOIViewController: UIViewController {
 
+    
+    @IBOutlet weak var locationTextField: UITextField!
+    @IBOutlet weak var countryTextField: UITextField!
+    @IBOutlet weak var clue1TextField: UITextField!
+    @IBOutlet weak var clue2TextField: UITextField!
+    @IBOutlet weak var clue3TextField: UITextField!
+   
+    @IBAction func cancelTapped(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func saveTapped(_ sender: Any) {
+        
+        guard let newLocation = locationTextField.text, let newCountry = countryTextField.text else { return }
+        
+        var clues: [String] = []
+        
+        if let clue1 = clue1TextField.text, !clue1.isEmpty {
+            clues.append(clue1)
+        }
+        
+        if let clue2 = clue2TextField.text, !clue2.isEmpty {
+            clues.append(clue2)
+        }
+        
+        if let clue3 = clue3TextField.text, !clue3.isEmpty {
+            clues.append(clue3)
+        }
+        
+        delegate?.poiWasAdded(POI(location: newLocation, country: newCountry, clues: clues))
+    }
+    
+    
+    var delegate: AddPOIDelegate?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -27,4 +66,10 @@ class AddPOIViewController: UIViewController {
     }
     */
 
+}
+
+extension AddPOIViewController: UITextFieldDelegate {
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        
+    }
 }
