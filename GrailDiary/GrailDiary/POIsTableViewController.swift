6e5413a7df64e521820e2838e0024a92b3786ada
Cody Morley <codymorleyinc@gmail.com>
2020-03-19 14:43:35 -0700

    finished code tasks in POIsViewController.swift

--- GrailDiary/GrailDiary/POIsTableViewController.swift
+++ GrailDiary/GrailDiary/POIsTableViewController.swift
@@ -2,35 +2,56 @@
 //  POIsTableViewController.swift
 //  GrailDiary
 //
-//  Created by Cody Morley on 3/17/20.
+//  Created by Bling Morley on 3/19/20.
 //  Copyright © 2020 Cody. All rights reserved.
 //
 
 import UIKit
 
-class ViewController: UIViewController {
+class POIsTableViewController: UIViewController {
 
-    
+    var pois: [POI] = []
+        
     @IBOutlet weak var tableView: UITableView!
     
-    var pois: [POI] = []
+    
+    
+    
+    
     
     override func viewDidLoad() {
+        
         super.viewDidLoad()
+
         // Do any additional setup after loading the view.
     }
+    
 
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
 
 }
 
-extension ViewController: UITableViewDataSource {
+extension POIsTableViewController: UITableViewDataSource, UITableViewDelegate {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        <#code#>
+        return pois.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        <#code#>
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "POICell", for: indexPath) as? POITableViewCell else { return UITableViewCell() }
+        let poi = pois[indexPath.row]
+        cell.poi = poi
+        
+        return cell
     }
     
     
 }
+
